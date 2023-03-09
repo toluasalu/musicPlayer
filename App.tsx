08d8458ae5c6f59fe460fbf3448a1846afd56e4e
toluasalu <johnasalu13@gmail.com>
2023-03-09 17:12:39 +0100

    Final changes

--- App.tsx
+++ App.tsx
@@ -1,48 +1,150 @@
+import React, { useEffect, useRef, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, TouchableOpacity, View, Dimensions } from 'react-native';
-import { Ionicons  } from '@expo/vector-icons';
-
+import { SafeAreaView, StyleSheet, TouchableOpacity, View, Dimensions, Image, Text, FlatList, Animated } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import Slider from '@react-native-community/slider';
 const { width, height } = Dimensions.get('window');
+import songs from './model/data';
 
 export default function App() {
+
+  const scrollX = useRef(new Animated.Value(0)).current;
+  const [songIndex, setSongIndex] = useState(0);
+  const songSlider = useRef(null);
+
+  useEffect(() => {
+    scrollX.addListener(({ value }) => {
+      const index = Math.round(value / width)
+      setSongIndex(index);
+    })
+
+    return () => {
+      scrollX.removeAllListeners();
+    }
+  }, []);
+
+  const skipToNext = () => {
+    songSlider.current?.scrollToOffset({
+      offset: (songIndex + 1) * width
+    })
+
+  }
+
+
+  const skipToPrevious = () => {
+    songSlider.current?.scrollToOffset({
+      offset: (songIndex - 1) * width
+    })
+  }
+
+
+
+  const renderSongs = ({ index, item }: any) => {
+    return (
+      <Animated.View style={{ width: width, justifyContent: 'center', alignItems: 'center' }}>
+        <View style={styles.artworkWrapper}>
+          <Image source={item.image} style={styles.artworkImg} />
+        </View>
+      </Animated.View>
+    )
+  }
+
+
   return (
 
     <SafeAreaView style={styles.container}>
       <View style={styles.mainContainer}>
-     
-     
-      
-    </View>
-    <View style={{
-      borderTopColor: '#393E46',
-      borderTopWidth: 1,
-      width: width,
-      alignItems: 'center',
-      paddingVertical: 15
-       }}>
+        <View style={{ width: width }}>
+          <Animated.FlatList
+            ref={songSlider}
+            data={songs}
+            renderItem={renderSongs}
+            horizontal
+            pagingEnabled
+            showsHorizontalScrollIndicator={false}
+            scrollEventThrottle={16}
+            onScroll={Animated.event(
+              [{
+                nativeEvent: {
+                  contentOffset: { x: scrollX }
+                }
+              }],
+              { useNativeDriver: true }
+            )}
+          />
+
+        </View>
+
+        <View>
+          <Text style={styles.title}>{songs[songIndex].title}</Text>
+          <Text style={styles.artist}>{songs[songIndex].artist}</Text>
+        </View>
+
+        <View>
+          <Slider
+            style={styles.progressContainer}
+            value={10}
+            minimumValue={0}
+            maximumValue={100}
+            thumbTintColor="#FFD369"
+            minimumTrackTintColor='#FFD369'
+            maximumTrackTintColor='#fff'
+            onSlidingComplete={() => { }}
+          />
+          <View style={styles.progressContainerLabel}>
+            <Text style={styles.ProgressLabelTxt}>0:00</Text>
+            <Text style={styles.ProgressLabelTxt}>3:55</Text>
+          </View>
+        </View>
+
+        <View style={styles.musicControls}>
+          <TouchableOpacity onPress={skipToPrevious}>
+            <Ionicons name='play-skip-back-outline' size={35} color='#FFD369' style={{ marginTop: 25 }} />
+          </TouchableOpacity>
+
+          <TouchableOpacity>
+            <Ionicons name='ios-pause-circle' size={75} color='#FFD369' />
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={skipToNext}>
+            <Ionicons name='play-skip-forward-outline' size={35} color='#FFD369' style={{ marginTop: 25 }} />
+          </TouchableOpacity>
+        </View>
+
+      </View>
+
+
+      <View style={{
+        borderTopColor: '#393E46',
+        borderTopWidth: 1,
+        width: width,
+        alignItems: 'center',
+        paddingVertical: 15
+      }}>
 
         <View style={{
-          flexDirection: 'row', 
-          justifyContent: 'space-between', 
-          width: '80%'}}>
-            <TouchableOpacity onPress = {() => {}}>
-             <Ionicons name='heart-outline' size={30} color= '#777777'/>
-            </TouchableOpacity>
-         
-            <TouchableOpacity onPress = {() => {}}>
-             <Ionicons name='repeat' size={30} color= '#777777'/>
-            </TouchableOpacity>
-            <TouchableOpacity onPress = {() => {}}>
-             <Ionicons name='share-outline' size={30} color= '#777777'/>
-            </TouchableOpacity>
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          width: '80%'
+        }}>
+          <TouchableOpacity onPress={() => { }}>
+            <Ionicons name='heart-outline' size={30} color='#777777' />
+          </TouchableOpacity>
 
-            <TouchableOpacity onPress = {() => {}}>
-             <Ionicons name='ellipsis-horizontal' size={30} color= '#777777'/>
-            </TouchableOpacity>
+          <TouchableOpacity onPress={() => { }}>
+            <Ionicons name='repeat' size={30} color='#777777' />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => { }}>
+            <Ionicons name='share-outline' size={30} color='#777777' />
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={() => { }}>
+            <Ionicons name='ellipsis-horizontal' size={30} color='#777777' />
+          </TouchableOpacity>
         </View>
-      
-    </View>
-    <StatusBar style="auto" />
+
+      </View>
+      <StatusBar style="auto" />
     </SafeAreaView>
   );
 }
@@ -53,9 +155,67 @@ const styles = StyleSheet.create({
     backgroundColor: '#222831',
   },
 
-  mainContainer : {
+  mainContainer: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  musicControls: {
+    marginTop: 15,
+    width: '70%',
+    justifyContent: 'space-between',
+    flexDirection: 'row'
+
+  },
+
+  artworkWrapper: {
+    width: 300,
+    height: 340,
+    marginBottom: 25,
+
+    shadowColor: '#ccc',
+    shadowOffset: {
+      width: 5,
+      height: 5,
+    },
+
+    shadowOpacity: 0.5,
+    shadowRadius: 3.84,
+    elevation: 5
+  },
+  artworkImg: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 15
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: '600',
+    textAlign: 'center',
+    color: '#EEEEEE'
+  },
+  artist: {
+    fontSize: 16,
+    fontWeight: '200',
+    textAlign: 'center',
+    color: '#EEEEEE'
+
+  },
+  progressContainer: {
+    width: 350,
+    height: 40,
+    marginTop: 25,
+    flexDirection: 'row'
+  },
+  progressContainerLabel: {
+    width: 340,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+
+  },
+  ProgressLabelTxt: {
+    color: '#fff'
   }
+
+
 });
---
+++ model/data.ts
@@ -0,0 +1,33 @@
+ const songs = [{
+    title:"O",
+    artist:"Coldplay",
+    image: require('../assets/coldplay.jpg'),
+    id: 1,
+},
+{
+    title:"Lucky",
+    artist:"Daft Punk",
+    image: require('../assets/daft-punk.jpg'),
+    id: 2,
+},
+{
+    title:"Nothing was the same",
+    artist:"Drake",
+    image: require('../assets/nothing.jpg'),
+    id: 3,
+},
+{
+    title:"Unknown",
+    artist:"Paramore",
+    image: require('../assets/paramore.jpg'),
+    id: 4,
+},
+{
+    title:"Money",
+    artist:"Wolves",
+    image: require('../assets/wolves.jpg'),
+    id: 5,
+},
+]
+
+export default songs;
